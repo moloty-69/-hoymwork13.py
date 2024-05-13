bae19e2278612e0b9510b2282ff55e82a751b405
moloty-69 <moloty-69@yandex.ru>
2024-05-13 12:33:39 +0300

    Add files via upload

---
+++ main 3.py
@@ -0,0 +1,27 @@
+
+#list_car=["BMW","MB","LADA","KIA","HONDA"]
+#for i in list_car:
+  #print("Я езжу на атамабиле марки " + i)
+  
+list_car = ["BMW","MB","LADA","KIA","HONDA"]
+print(list_car)
+cars_count = 0
+for i in list_car:
+cars_count += 10
+print(cars_count)
+print("Я езжу на автомобиле марки" + i)
+  
+   
+
+
+
+  
+  
+    
+
+     
+  
+
+  
+  
+ 
